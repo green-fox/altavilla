ef165e2881b3a6036e3a14fa1bf43fc8358f9037
green-fox <schreiber.alexis.as@gmail.com>
2015-01-07 19:52:34 +0100

    modify the blog appi to add the concert

--- blog/models.py
+++ blog/models.py
@@ -40,6 +40,21 @@ class Article(models.Model):
 			print("exception 3 : remove failed")
 	def __unicode__(self):
 		return self.title
+
+
+
+class Concert(models.Model):
+
+	date=models.DateTimeField(verbose_name="Date du concert (*) :")
+#	datetime.widget = forms.SpdlitDateTimeWidget(time_format=('%H:%M'))
+	paf=models.DecimalField(verbose_name="Prix place  :",max_digits=5,decimal_places=0,blank=True,null=True)
+
+	description=models.CharField(verbose_name="Details (*) :",max_length=50)
+	place=models.CharField(verbose_name="Lieu (*) :",max_length=50)
+	models.help_text="Enter the minimum (inclusive) value for this concept."
+	def __unicode__(self):
+		return self.description	
+
 #def delete_file_field(sender, **kwargs):
 #    print("delete in progress")
 #    """Automatically deleted files when records removed.
