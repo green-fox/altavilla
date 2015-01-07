2bb0f888fc587d49bae6351a20dfaa0c66ecae61
green-fox <schreiber.alexis.as@gmail.com>
2015-01-07 22:18:50 +0100

    v1

--- blog/views.py
+++ blog/views.py
@@ -5,8 +5,11 @@ from blog.models import Article, Concert
 # Create your views here.
 def index(request):	
 	article_list= Article.objects.order_by('-creation_date')
-	context = {'article_list': article_list}
+	concert_list= Concert.objects.order_by('paf')
+	context = {'article_list': article_list, 'concert_list' : concert_list }
 	return render(request, 'blog/index.html', context)
+
+
 
 
 
