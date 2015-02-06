6f0266d1150d52d65e5fd92962d332bbf0347638
green-fox <scheiber.alexis.as@gmail.com>
2015-02-06 23:37:04 +0100

    broken menu template

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from blog.models import Article, Song, Carousel
+from blog.models import Article, Song, Carousel, Concert
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
@@ -8,8 +8,9 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 def index(request):	
 	carousel_pic = Carousel.objects.order_by('picture')
         song_list =  Song.objects.order_by('song_title')
-	article_list= Article.objects.order_by('-creation_date')
-        context = {'article_list': article_list, 'song_list': song_list, 'carousel_pic': carousel_pic}
+	article_list = Article.objects.order_by('-creation_date')
+	concert_list = Concert.objects.order_by('-date')
+        context = {'article_list': article_list, 'song_list': song_list, 'carousel_pic': carousel_pic, 'concert_list' : concert_list}
 	return render(request, 'blog/index.html', context)
 
 
